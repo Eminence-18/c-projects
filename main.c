16008577390e13129caa51e5cc20b9cd0f6ba461
Eminence-18 <2406531@students.kcau.ac.ke>
2025-04-02 20:54:33 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,77 +1,33 @@
+#include <iso646.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-struct Patient {
-    char name[50];
-    int age;
-    char disease[50];
-    char appointment_time[20];
-};
+int main(void) {
+    float quiz,mid_x,final_x,average;
 
-void addPatient() {
-    FILE *fptr;
-    struct Patient p;
+    printf("enter quix score (out of 100)");
+    scanf("%f",&quiz);
 
-    fptr = fopen("patients.txt", "a");
+    printf("enter mid x (out of 100)");
+    scanf("%f",&mid_x);
 
-    printf("Enter patient's name:\n");
-    scanf("%s", p.name);
-    printf("Enter patient's age:\n");
-    scanf("%d", &p.age);
-    printf("Enter disease:\n");
-    scanf("%s", &p.disease);
-    printf("Enter appointment time:\n");
-    scanf("%s", &p.appointment_time);
+    printf("enter final x (out of 100)");
+    scanf("%f",&final_x);
 
-    fprintf(fptr, "Name: %s\nAge: %d\nDisease: %s\nAppointment Time: %s\n\n", p.name, p.age, p.disease, p.appointment_time);
+    average=(quiz+mid_x+final_x)/3;
 
-    fclose(fptr);
-    printf("Patient added successfully!\n");
-}
+    if(average>90)
+        printf("\ngrade is A");
 
-void viewPatients() {
-    FILE *fptr;
-    char ch;
+    if(average>=70 &&  average<90 )
+        printf("\ngrade is B");
 
-    fptr = fopen("patients.txt", "r");
+    if (average>=50 && average <70)
+        printf("\ngrade is C");
 
-    if (fptr == NULL) {
-        printf("No records found.\n");
-        return;
-    }
+    if (average <50)
+        printf("\ngrade is f");
 
-    while ((ch = fgetc(fptr)) != EOF) {
-        putchar(ch);
-    }
 
-    fclose(fptr);
-}
-
-int main() {
-    int choice;
-
-    while (1) {
-        printf("\nHospital Management System\n");
-        printf("1. Add Patient\n");
-        printf("2. View Patients\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                addPatient();
-            break;
-            case 2:
-                viewPatients();
-            break;
-            case 3:
-                exit(0);
-            default:
-                printf("Invalid choice! Please try again.\n");
-        }
-    }
 
     return 0;
 }
